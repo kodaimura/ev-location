0615e8d378412741539dd9de531e685e630bff92
kodaimura <kodaimura123@gmail.com>
2024-12-27 21:18:32 +0900

    add some pages

--- routes.jl
+++ routes.jl
@@ -1,5 +1,13 @@
 using Genie.Router
 
 route("/") do
-  serve_static_file("welcome.html")
+  serve_static_file("index.html")
+end
+
+route("/login") do
+  serve_static_file("login.html")
+end
+
+route("/signup") do
+  serve_static_file("signup.html")
 end
