ee7afdfad847182e920c57779efd0201205d6b58
kodaimura <kodaimura123@gmail.com>
2025-01-12 18:19:11 +0900

    deploy

--- config/env/prod.jl
+++ config/env/prod.jl
@@ -1,7 +1,7 @@
 using Genie, Logging
 
 Genie.Configuration.config!(
-  server_port                     = 8000,
+  server_port                     = 3800,
   server_host                     = (Sys.iswindows() ? "127.0.0.1" : "0.0.0.0"),
   log_level                       = Logging.Error,
   log_to_file                     = true,
