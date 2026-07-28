70da378f5bae6f7757c5f9ea8d86edf6a3ccc1b3
kodaimura <kodaimura123@gmail.com>
2026-07-29 01:20:22 +0900

    refactor: production database settings from environment

--- config/initializers/searchlight.jl
+++ config/initializers/searchlight.jl
@@ -11,7 +11,25 @@ function StructTypes.StructType(::Type{SearchLight.DbId})
   StructTypes.Struct()
 end
 
+function override_db_config_from_env!()
+  settings = SearchLight.config.db_config_settings
+  overrides = Dict(
+    "database" => "DB_NAME",
+    "host" => "DB_HOST",
+    "username" => "DB_USER",
+    "password" => "DB_PASSWORD",
+    "port" => "DB_PORT"
+  )
+
+  for (config_key, env_key) in overrides
+    value = get(ENV, env_key, "")
+    isempty(value) && continue
+    settings[config_key] = config_key == "port" ? parse(Int, value) : value
+  end
+end
+
 if ENV["GENIE_ENV"] != "test"
   SearchLight.Configuration.load(context = @__MODULE__)
+  override_db_config_from_env!()
   SearchLight.connect()
 end
